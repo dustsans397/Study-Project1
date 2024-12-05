cdf8769a883de14672a1b32a5f29924f045a510c
dustsans397 <3080524465@qq.com>
2024-12-05 12:59:50 +0800

    move

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -9,13 +9,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int num, score, x = 0, y = 0;
-        Debug.Log("请输入人数：");
-        num=Console.Read();
-        for (int i = 1; i < num; i++)
-        {
-            
-        }
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
